e1d0e3df0976a0ec5849a264fa26f4394abc49c6
Manisha A <96335649+ManishaYoga@users.noreply.github.com>
2024-08-25 18:29:44 +0530

    Delete Week_4_Exercises/Week_4_Exercises directory

--- Week_4_Exercises/Week_4_Exercises/Task 6,7/src/main/java/com/library/BookStoreAPI/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.library.BookStoreAPI;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
